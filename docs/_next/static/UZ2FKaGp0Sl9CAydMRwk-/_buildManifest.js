b3b40a064d9c725daa91efd9be4140f3a49c41c1
Dev Hau <50167033+devhau@users.noreply.github.com>
2022-05-19 16:36:04 +0700

    update

---
+++ docs/_next/static/UZ2FKaGp0Sl9CAydMRwk-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/102-c057578dd43c436e.js","static/chunks/pages/index-7792fa18f11661fb.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
