3a27223575e5812d82ba93525b67cfe97fec7f25
Dev Hau <50167033+devhau@users.noreply.github.com>
2022-05-19 18:29:02 +0700

    up

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
   site_name: 'visualflow',
   title: 'Visual Flow Creator',
-  description: 'Visual Flow Creator',
+  description: `Visual Flow Creator Support for framework reactjs, vue, angular, html.`,
+  canonical: "https://visualflow.cc/",
   locale: 'en',
 };
